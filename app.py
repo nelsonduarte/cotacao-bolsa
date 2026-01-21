7bb2bd33ae7b66017b5762b32573e4236cd32708
Nelson Duarte <nelson.miguel.duarte@outlook.com>
2026-01-21 11:56:04 +0000

    Inserção de um loop while

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
 import pandas as pd
 import yfinance as yf
 
-nome_acao = input("Insira o nome da ação em bolsa que deseja obter informações: ")
-data_inicio = input("Insira uma data de inicio: ")
-data_fim = input("Insira uma data de fim: ")
+opcao = "" # variavel de controle
 
-# obter os dados de cotação em bolsa 
-df = yf.download(nome_acao, start=data_inicio, end=data_fim)
-print(df)
+while True:
+    print("\n-------- Cotação de Ações --------\n")
+    
+    nome_acao = input("Insira o nome da ação em bolsa que deseja obter informações: ")
+    data_inicio = input("Insira uma data de inicio: ")
+    data_fim = input("Insira uma data de fim: ")
+
+    # obter os dados de cotação em bolsa 
+    df = yf.download(nome_acao, start=data_inicio, end=data_fim)
+    print(df)
+
+    opcao = input("\nDeseja continuar? (s/n) ").lower()
+
+    if opcao == 'n':
+        break
+

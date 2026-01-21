92848a52dfc64fe6efc3b637c52639acd014c074
Nelson Duarte <nelson.miguel.duarte@outlook.com>
2026-01-21 11:37:55 +0000

    alteração da aplicação com a inserção de inputs para o nome da ação e datas de inicio e fim

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 import pandas as pd
 import yfinance as yf
 
+nome_acao = input("Insira o nome da ação em bolsa que deseja obter informações: ")
+data_inicio = input("Insira uma data de inicio: ")
+data_fim = input("Insira uma data de fim: ")
+
 # obter os dados de cotação em bolsa 
-df = yf.download('AAPL', start='2025-12-01', end='2025-12-31')
+df = yf.download(nome_acao, start=data_inicio, end=data_fim)
 print(df)
